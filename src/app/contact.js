176f4775fa1ceace8db639a5f9d3eaf8776b7a33
omm-narayan <omm.codes@gmail.com>
2025-10-15 20:07:38 +0530

    update

---
+++ src/app/contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h2>Contact Us</h2>
+      <p>Have questions? Reach out to us!</p>
+      
+      <p>
+        Phone: <a href="tel:+911234567890">+91 1234567890</a>
+      </p>
+      <p>
+        Email: <a href="mailto:info@feynmanrobotics.com">info@feynmanrobotics.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default ContactUs;
